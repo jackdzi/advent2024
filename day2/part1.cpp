4575b31521a9fdc5feaa752daa0538b3b8450287
jackdzi <jackdzialoa@gmail.com>
2024-12-11 03:20:25 -0600

    day2

---
+++ day2/part1.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main(void) {
+  std::ifstream file;
+  file.open("data.txt");
+  if (!file.is_open()) {
+    return 1;
+  }
+  std::string line;
+  int safe_sum = 0;
+  int counter = 1;
+  while (std::getline(file, line)) {
+    std::stringstream sline(line);
+    std::string word;
+    sline >> word;
+    int previous = stoi(word);
+    int increasing = 0;
+    bool safe = true;
+    while (sline >> word) {
+      int diff = previous - stoi(word);
+      if (increasing == 0)
+        increasing = diff < 0 ? 1 : -1;
+      if ((increasing == 1 && diff > 0 ) || (increasing == -1 && diff < 0)) {
+        safe = false;
+        break;
+      } else if (abs(diff) > 3 || diff == 0) {
+        safe = false;
+        break;
+      }
+      previous = stoi(word);
+    }
+    if (safe)
+      safe_sum++;
+  }
+  std::cout << safe_sum;
+  return 0;
+}
